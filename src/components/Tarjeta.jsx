939331b61599d28d70ced9fb356a11ed0e40a0fa
José Battaglia <battagliajose@gmail.com>
2024-05-24 20:42:53 -0300

    Carga de tarjetas de alojamientos desde json

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom"; 
 
-function Tarjeta(props) {
+function Tarjeta(alojamiento) {
     return( 
         <div className="card">
-        <img src={props.img} alt="card" />
+        <img src={alojamiento.imagen} alt="card" />
         <div className="cardContenido">
-            <h4 className="cardTitulo">{props.titulo}</h4>
-            <p className="cardDescripcion">{props.descrip}</p>
+            <h4 className="cardTitulo">{alojamiento.titulo}</h4>
+            <p className="cardDescripcion">{alojamiento.descripcion}</p>
             <div className="cardPie">
-                <p className="cardPieOferta">Oferta! {props.oferta}</p>
+                <p className="cardPieOferta">Oferta! {alojamiento.oferta}</p>
                 <Link className="cardPieBoton" to="/">¡Reserva Ya!</Link> 
             </div>
         </div>
