da336828379f54c077a76f0f165b226cc217dbb3
Jose A. Battaglia <battagliajose@gmail.com>
2024-05-24 09:30:30 -0300

    Agregada Maqueta Portada y Tarjetas

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import '../styles/portada.css'
+import portadaImagen from '../assets/portada/friends-planning-travel-looking-at-map-article-032822_wide.jpg'
 
 function Portada(props) {
     return( 
-        <div>
-            <p>Portada</p>
-        </div>
+        <section class="portada">
+            <div class="contenedorImagenPortada">
+                <img class="imagenPortada" src={portadaImagen} alt="friends friends-planning-travel-looking-at-map-article-032822" />
+                <div class="botonera">
+                    <input class="inputPortada" type="text" placeholder="Donde quieres viajar?" />
+                    <input class="inputPortada" type="text" placeholder="Elije una fecha.." />
+                    <div class="btnPortada">Encontrar!</div>
+                </div>
+            </div>
+            <div class="espaciador">
+                <img class="imagenPortadaEspaciador" src={portadaImagen} alt="friends friends-planning-travel-looking-at-map-article-032822" />
+            </div>
+        </section>
     );
 }
 
--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import apartmentImage from '../assets/tarjetas/apartment-01.jpg';
 
 function Tarjeta(props) {
     return( 
-        <div>
-            <p>Tarjeta</p>
+        <div className="card">
+        <img src={apartmentImage} alt="card" />
+        <div className="cardContenido">
+            <h4 className="cardTitulo">Bella Vista</h4>
+            <p className="cardDescripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque praesentium repellat magnam laudantium nisi!</p>
+            <div className="cardPie">
+                <p className="cardPieOferta">Oferta! 35% off</p>
+                <a className="cardPieBoton" href="\">¡Reserva Ya!</a> 
+            </div>
         </div>
+    </div>
     );
 }
 
