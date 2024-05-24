a5fb1572812abd4f77823e32a1dd7c478d06d14c
Jose A. Battaglia <battagliajose@gmail.com>
2024-05-24 09:54:33 -0300

    Agregado componente para error 404 o ruta no encontrada

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import '../styles/notfound.css'
+import image404 from '../assets/notfound/404.png'
 
 function NotFound(props){
     return (
-        <h1>404 - Página no encontrada</h1>
+        <div>
+            <img src={image404} alt="404 - Página no encontrada" />
+        </div>
     );
 }
 
