0b8db053d6149909b4e2b8b13d404382e7e3b338
cristianseltenreich <cristianseltenreich@gmail.com>
2024-05-25 16:31:53 -0300

    componente NavBar-v1.01 y sección3beneficios-v0.01

--- src/components/Beneficios.jsx
+++ src/components/Beneficios.jsx
@@ -1,11 +1,66 @@
-import React from "react";
+import '../styles/beneficios.css'
+import '../styles/seccion3.css'
+import React from 'react'
 
-function Beneficios(props) {
-    return( 
-        <div>
-            <p>Beneficios</p>
-        </div>
-    );
+const Beneficios = () => {
+    return (
+
+        <section class="beneficios">
+            <div class="container bg-dark text-white pb-3">
+                <div class="row">
+                    <div class="col-md justify-content-start align-items-center">
+                        <h2>Millones de opciones para tus <span class="rojito">vacaciones inolvidables</span> alrededor del mundo!</h2>
+                        <p>Descubre la comodidad de nuestros alojamientos vacacionales.</p>
+                    </div>
+                    <div class="col-md justify-content-start align-items-center">
+                        <p>Disfruta de servicios excepcionales, ubicaciones privilegiadas y las mejores tarifas.</p>
+                        <p><strong>¡Reserva ahora para una <span class="rojito">experiencia única</span>!</strong></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center container-beneficios">
+                        <div class="d-flex justify-content-center">
+                            <div class="square-container">
+                                <i class="bi bi-building-fill icon-style-pers"></i>
+                            </div>
+                            <div class="square-container">
+                                <i class="bi bi-car-front-fill icon-style-pers"></i>
+                            </div>
+                            <div class="square-container">
+                                <i class="bi bi-airplane-fill icon-style-pers"></i>
+                            </div>
+                            <div class="square-container">
+                                <i class="bi bi-bus-front-fill icon-style-pers"></i>
+                            </div>
+                            <div class="square-container">
+                                <i class="bi bi-suitcase2-fill icon-style-pers"></i>
+                            </div>
+                            <div class="square-container">
+                                <i class="bi bi-brightness-alt-high-fill icon-style-pers"></i>
+                            </div>
+                            <div class="square-container">
+                                <i class="bi bi-camera-fill icon-style-pers"></i>
+                            </div>
+                            <div class="square-container">
+                                <i class="bi bi-train-freight-front-fill icon-style-pers"></i>
+                            </div>
+                            <div class="square-container">
+                                <i class="bi bi-compass icon-style-pers"></i>
+                            </div>
+                            <div class="square-container">
+                                <i class="bi bi-passport icon-style-pers"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+    )
 }
 
-export default Beneficios;
+export default Beneficios
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,24 @@
 import '../styles/navbar.css'
 import React from "react";
 import { Link } from "react-router-dom"; 
-
-   /* <div classNameName="navbar">
-            <Link to="/">Home -</Link>
-            <Link to="/nosotros">Nosotros -</Link>
-            <Link to="/contacto">Contacto</Link>
-        </div>*/
-
+  
 const NavBar = () => { 
     
     return(     
         <>
             <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
                 <div className="container">                  
-                  <h1><Link to="/">hotelFlix</Link></h1>
+                  <h1><Link to="/" class="navbar-brand h1_header">hotelFlix</Link></h1>
                   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                     <span className="navbar-toggler-icon"></span>
                   </button>
                   <div className="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
                     <ul className="navbar-nav nav-pers">
                       <li className="nav-item">
-                        <Link to="/" className="nav-pers-link nav-link">Home -</Link>                        
+                        <Link to="/" className="nav-pers-link nav-link">Home</Link>                        
                       </li>
                       <li className="nav-item">
-                        <Link to="/nosotros" className="nav-pers-link nav-link">Nosotros -</Link>
+                        <Link to="/nosotros" className="nav-pers-link nav-link">Nosotros</Link>
                       </li>
                       <li className="nav-item">
                         <Link to="/contacto" className="nav-pers-link nav-link">Contacto</Link>                        
