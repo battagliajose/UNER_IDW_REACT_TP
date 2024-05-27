33efbf548caa0bf14081a6bea3556374f9363d81
Jose A. Battaglia <battagliajose@gmail.com>
2024-05-27 11:00:25 -0300

    Agregada página admin (vacia)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//Bootstrap
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+//CSS
 import './styles/reset.css';
 import './styles/normalize.css';
 import './styles/main.css';
+
+//Componentes
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+
+//Páginas
 import Home from './pages/Home';
 import Contacto from './pages/Contacto';
 import Nosotros from './pages/Nosotros';
-import Footer from './components/Footer';
+import Admin from './pages/Admin';
 import NotFound from './components/NotFound';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
@@ -24,6 +33,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/contacto" element={<Contacto />} />
               <Route path="/nosotros" element={<Nosotros />} />
+              <Route path="/admin" element={<Admin />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
         </main>
---
+++ src/pages/Admin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Admin () {
+    return (
+        <div>
+            <h1 style={{margin: "300px"}}>ADMIN</h1>
+        </div>
+    );
+}
+
+export default Admin;
