5cda1133ebe1bf7595db2e8c963c214ac8a9bdb7
luis782006 <luisfelipe782006@gmail.com>
2024-05-28 20:59:42 -0300

    Formulario

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import '../styles/styleform.css'
 
-function Admin () {
+const Admin =()=> {
+
+    
+const [descripcion,setDescripcion]=useState('');
+
+const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDescripcion({              
+      [name]: value,
+    });
+  };
+const envioTipo = async (e) => { 
+    e.prevent();
+    const dataJson={
+        Descricpion: descripcion
+    };
+    //envio del json
+    try {
+        
+        const response =await fetch('http://localhost:3001/tiposAlojamiento/createTipoAlojamiento', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataJson)
+        });
+        if(response.ok){
+            alert('Tipo alojamiento creado');
+        }
+    } catch (error) {
+        alert('Error: ' + error.message);
+    }
+};
+
+
+
+
     return (
         <div>
             <h1 style={{margin: "300px"}}>ADMIN</h1>
+            <div className="form-container">
+                <h2>Tipos de alojamientos</h2>
+                <form onSubmit={envioTipo}>
+                    <div>
+                        <label htmlFor="descripcion">Tipo Alojamiento:</label>
+                        <input
+                        type="text"
+                        id="descripcion"
+                        name="descripcion"
+                        value={descripcion}
+                        onChange={handleChange}
+                        required
+                        />
+                    </div>
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
         </div>
     );
 }
