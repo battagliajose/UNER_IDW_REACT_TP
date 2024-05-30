1d804dda05212158b1064c8ec0db8edc0994df63
Jose A. Battaglia <battagliajose@gmail.com>
2024-05-30 13:05:26 -0300

    Corregido Create de Tipo Alojamientos - Queda Funcionando

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,46 +1,36 @@
 import React, { useState } from "react";
-import '../styles/styleform.css'
+import '../styles/admin.css'
 
 const Admin =()=> {
-
     
-const [descripcion,setDescripcion]=useState('');
+    const [descripcion, setDescripcion] = useState('');
 
-const handleChange = (e) => {
-    const { name, value } = e.target;
-    setDescripcion({              
-      [name]: value,
-    });
-  };
-const envioTipo = async (e) => { 
-    e.prevent();
-    const dataJson={
-        Descricpion: descripcion
-    };
-    //envio del json
-    try {
-        
-        const response =await fetch('http://localhost:3001/tiposAlojamiento/createTipoAlojamiento', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dataJson)
-        });
-        if(response.ok){
-            alert('Tipo alojamiento creado');
+    const envioTipo = async (event) => { 
+        event.preventDefault();
+
+        const dataJson = {
+            Descripcion: descripcion
+        };
+
+        // Envio del json
+        try {
+            const response = await fetch('http://localhost:3001/tiposAlojamiento/createTipoAlojamiento', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dataJson)
+            });
+            if(response.ok){
+                alert('Tipo alojamiento creado');
+            }
+        } catch (error) {
+            alert('Error: ' + error.message);
         }
-    } catch (error) {
-        alert('Error: ' + error.message);
-    }
-};
-
-
-
+    };
 
     return (
         <div>
-            <h1 style={{margin: "300px"}}>ADMIN</h1>
             <div className="form-container">
                 <h2>Tipos de alojamientos</h2>
                 <form onSubmit={envioTipo}>
@@ -50,9 +40,8 @@ const envioTipo = async (e) => {
                         type="text"
                         id="descripcion"
                         name="descripcion"
-                        value={descripcion}
-                        onChange={handleChange}
                         required
+                        onChange={(e) => setDescripcion(e.target.value)}
                         />
                     </div>
                     <button type="submit">Enviar</button>
