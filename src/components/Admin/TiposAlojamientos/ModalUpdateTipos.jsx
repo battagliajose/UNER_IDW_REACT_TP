4fe2a2dedd755c1b2e5b17ed903b9342253fa0d9
luis782006 <luisfelipe782006@gmail.com>
2024-06-02 19:37:36 -0300

    Correccion de valor del input para editar

--- src/components/Admin/TiposAlojamientos/ModalUpdateTipos.jsx
+++ src/components/Admin/TiposAlojamientos/ModalUpdateTipos.jsx
@@ -3,99 +3,84 @@ import { Modal, Button, Form } from 'react-bootstrap';
 import InputGroup from 'react-bootstrap/InputGroup';
 import '../../../styles/modales.css';
 
-function ModalUpdateTipos({ show, handleClose, fetchTiposAlojamiento, id,descrip}) {
-
-  
+function ModalUpdateTipos({ show, handleClose, fetchTiposAlojamiento, id, descrip }) {
   const [validated, setValidated] = useState(false);
+  const [snack, setSnack] = useState(false);
+  const [descripcion, setDescripcion] = useState(""); // Inicializa descripción vacía
 
-  const[snack, setSnack]=useState(false);
+  useEffect(() => {
+    // Actualiza el estado de descripción con el valor de descrip cuando el componente se monta o cuando descrip cambia
+    setDescripcion(descrip);
+  }, [descrip]);
 
   const handleSubmit = (event) => {
-
-  //validacion del input
-  const form = event.currentTarget;
-  if (form.checkValidity() === false) {
-    event.preventDefault();
-    event.stopPropagation();
-  }else{
-    setValidated(true);  
-    event.preventDefault();
-    createTipoModal()
-    console.log(descripcion);
-    handleClose();
-    fetchTiposAlojamiento();
-    setSnack(true);
-    // Oculto el snack después de 3 segundos
-    setTimeout(() => {
-      setSnack(false);
-    }, 2000);
-    setValidated(false);  
-    }              
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      setValidated(true);
+      event.preventDefault();
+      createTipoModal();
+      console.log(descripcion);
+      handleClose();
+      fetchTiposAlojamiento();
+      setSnack(true);
+      setTimeout(() => {
+        setSnack(false);
+      }, 2000);
+      setValidated(false);
+    }
   };
 
-  const [descripcion, setDescripcion] = useState("descripcionMod");
-
-  const createTipoModal = async () => { 
-
+  const createTipoModal = async () => {
     const dataJson = {
-        Descripcion: descripcion
+      Descripcion: descripcion,
     };
 
     try {
-        const response = await fetch(`http://localhost:3001/tiposAlojamiento/putTipoAlojamiento/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dataJson)
-        });
-        if(response.ok){
-           
-            fetchTiposAlojamiento()
-        }
+      const response = await fetch(`http://localhost:3001/tiposAlojamiento/putTipoAlojamiento/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataJson),
+      });
+      if (response.ok) {
+        fetchTiposAlojamiento();
+      }
     } catch (error) {
-        alert('Error: ' + error.message);
+      alert('Error: ' + error.message);
     }
-
-};
+  };
 
   return (
     <>
-    <Modal className='modal-blur' show={show} onHide={handleClose}
-    aria-labelledby="contained-modal-title-vcenter"
-    centered
-    >     
-      <Modal.Body className='form-modal'>
-      <p>Tipo de alojamiento</p>
-         <Form  noValidate validated={validated} onSubmit={handleSubmit}>
-          <Form.Group controlId="formDescription">
-            <Form.Label>Descripción</Form.Label>
-            <InputGroup hasValidation>  
-            <Form.Control
-              value={descrip}
-              type="text"
-              required
-              placeholder="Ingresa una descripción"
-              onChange={(e) => setDescripcion(e.target.value)}
-            />
-          <Button className='button-cancelar' onClick={handleClose}>
-           Cancelar
-          </Button>
-          <Button className='button-Aceptar' onClick={handleSubmit}>
-            Aceptar
-          </Button>
-            <Form.Control.Feedback type="invalid">
-                    Debe llenar este campo
-            </Form.Control.Feedback>
-            </InputGroup>
-            </Form.Group>       
-        </Form>
-      </Modal.Body>     
-    </Modal>
-     <div className={snack ? 'mostrarSnack' : 'ocultarSnack'} >Tipo de alojamiento editado</div>
-     </>
+      <Modal className='modal-blur' show={show} onHide={handleClose} aria-labelledby="contained-modal-title-vcenter" centered>
+        <Modal.Body className='form-modal'>
+          <p>Tipo de alojamiento</p>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group controlId="formDescription">
+              <Form.Label>Descripción</Form.Label>
+              <InputGroup hasValidation>
+                <Form.Control
+                  value={descripcion}
+                  type="text"
+                  required
+                  placeholder="Ingresa una descripción"
+                  onChange={(e) => setDescripcion(e.target.value)}
+                />
+                <Button className='button-cancelar' onClick={handleClose}>Cancelar</Button>
+                <Button className='button-Aceptar' onClick={handleSubmit}>Aceptar</Button>
+                <Form.Control.Feedback type="invalid">Debe llenar este campo</Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
+      <div className={snack? 'mostrarSnack' : 'ocultarSnack'}>Tipo de alojamiento editado</div>
+    </>
   );
 }
-
 
 export default ModalUpdateTipos;
