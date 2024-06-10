e4662c3cc13341883a0face7c32bae5508935ca6
José Battaglia <battagliajose@gmail.com>
2024-06-10 10:14:38 -0300

    Se agrega Switch en lugar de ternaria para agregar CRUD Servicios

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,26 +1,31 @@
 import React, { useState, useEffect } from "react";
-import TablaTipos from "../components/Admin/TiposAlojamientos/TablaTipos";
-import ModalCreateTipos from "../components/Admin/TiposAlojamientos/ModalCreateTipos";
 import "../styles/admin.css";
-import SideBar from "../components/Admin/TiposAlojamientos/Sidebar";
-import CrudTipos from "../components/Admin/TiposAlojamientos/CrudTipos"
-import Alojamiento from "../components/Admin/TiposAlojamientos/Alojamiento";
+import SideBar from "../components/Admin/Sidebar";
+import CrudTipos from "../components/Admin/CRUD_TiposAlojamientos/CrudTipos"
+import CrudAlojamientos from "../components/Admin/CRUD_Alojamientos/CrudAlojamientos";
+import CrudServicios from "../components/Admin/CRUD_Servicios/CrudServicios"
 
 function Admin() {
   const [crudToShow, setCrudToShow] = useState(0);
 
+  const renderComponent = () => {
+    switch (crudToShow) {
+      case 0:
+        return <CrudAlojamientos />;
+      case 1:
+        return <CrudTipos />;
+      case 2:
+        return <CrudServicios />;
+      default:
+        return <CrudAlojamientos />;
+    }
+  }
+
    return (
     <div className="adminContainer">
           <SideBar setCrudToShow={setCrudToShow}/>
-          {(crudToShow===1) ? 
-          <div className="contenedores-components">
-            <CrudTipos />
-          </div> : 
-          <div className="contenedores-components">
-            <Alojamiento />
-          </div>}
+          {renderComponent()}
     </div>
-
   );
 }
 
