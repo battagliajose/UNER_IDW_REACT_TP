c99c71da3d53fa13d7be1e9093732bc7098be0da
cristianseltenreich <cristianseltenreich@gmail.com>
2024-06-22 10:59:17 -0300

    modificación navbar: toggle acordeón funcionando, agrego transparencia al fondo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,56 +1,56 @@
-import "../styles/navbar.css";
-import React from "react";
-import "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/navbar.css'; // Ajusta la ruta si es necesario
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <>
-      <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-        <div className="container">
-          <h1>
-            <Link to="/" className="h1_header navbar-brand">
-              hotelFlix
-            </Link>
-          </h1>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapsibleNavbar"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse justify-content-center"
-            id="collapsibleNavbar"
-          >
-            <ul className="navbar-nav nav-pers">
-              <li className="nav-item">
-                <Link to="/" className="nav-pers-link nav-link">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/nosotros" className="nav-pers-link nav-link">
-                  Nosotros
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/contacto" className="nav-pers-link nav-link">
-                  Contacto
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/admin" className="nav-pers-link nav-link">
-                  Admin
-                </Link>
-              </li>
-            </ul>
-          </div>
+    <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top navbar-transparent">
+      <div className="container">        
+        <Link to="/" className="navbar-brand">
+          <h1 className='h1_header'>hotelFlix</h1>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleNavbar}
+          aria-controls="navbarNav"
+          aria-expanded={isOpen}
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse justify-content-center ${isOpen ? 'show' : ''}`} id="navbarNav">
+          <ul className="nav-pers navbar-nav">
+            <li className="nav-item">
+              <Link to="/" className="nav-pers-link nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/nosotros" className="nav-pers-link nav-link">
+                Nosotros
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contacto" className="nav-pers-link nav-link">
+                Contacto
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/admin" className="nav-pers-link nav-link">
+                Admin
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
