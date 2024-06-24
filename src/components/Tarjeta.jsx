470f25461ef2dfbcd650a62dab2fc4c2544e8389
José Battaglia <battagliajose@gmail.com>
2024-06-24 20:01:49 -0300

    Agregado Estado en tarjetas

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -7,7 +7,7 @@ import HeartFull from "../assets/tarjetas/heart-Full.png";
 import bath from "../assets/tarjetas/banera.png";
 import dorm from "../assets/tarjetas/dormitorio.png";
 
-function Tarjeta({ alojamiento: {PrecioPorDia, CantidadDormitorios, CantidadBanios, Titulo, Descripcion}, image, serviciosAlojamiento }) {
+function Tarjeta({ alojamiento: {PrecioPorDia, CantidadDormitorios, CantidadBanios, Titulo, Descripcion, Estado}, image, serviciosAlojamiento }) {
   const [like, setLike] = useState(false);
 
   const serviciosAlojamientoConValor = serviciosAlojamiento && serviciosAlojamiento.length > 0 
@@ -16,11 +16,14 @@ function Tarjeta({ alojamiento: {PrecioPorDia, CantidadDormitorios, CantidadBani
 
   return (
     <div className="tarjeta">
-      <img src={image} alt="tarjeta" />
-      <div className="tarjetafavoriteIcon" onClick={(e) => setLike(!like)}>
-        <img src={like ? HeartFull : HeartTransp} alt="favorito" />
+      <div className="tarjetaImageContainer">
+        <img src={image} alt="tarjeta" />
+        <div className="tarjetafavoriteIcon" onClick={(e) => setLike(!like)}>
+          <img src={like ? HeartFull : HeartTransp} alt="favorito" />
+        </div>
+        <p className="tarjetaPrecioDia">${PrecioPorDia}</p>
+        <p className="tarjetaEstado">{Estado}</p>  
       </div>
-      <p className="tarjetaPrecioDia">${PrecioPorDia}</p>
       <div className="tarjetaInfoIcons">
         <img src={dorm} alt="Dormitorios" />
         <p>{CantidadDormitorios}</p>
@@ -44,7 +47,7 @@ function Tarjeta({ alojamiento: {PrecioPorDia, CantidadDormitorios, CantidadBani
               ))}
             </DropdownMenu>
           </Dropdown>
-                  
+                          
           <p className="tarjetaPieOferta"></p>
           <Link className="tarjetaPieBoton" to="/contacto">
             ¡Reserva Ya!
